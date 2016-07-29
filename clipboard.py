0f552d13a43eee29cbff8ad8ecfe5113ff76e7e9
SAMUEL KUSSWURM <Sam@SAMUELs-MacBook-Pro.local>
2016-07-28 22:11:22 -0500

    Tyrion is alive and well

---
+++ clipboard.py
@@ -0,0 +1,10 @@
+tyrion_drinking_episode = {}
+
+def add_drinking_episode(episode, number_drinks):
+	tyrion_drinking_episode[add_drinking_episode[episode]] = number_drinks
+
+
+add_drinking_episode('S4E9', '3')
+
+print(tyrion_drinking_episode)
+
